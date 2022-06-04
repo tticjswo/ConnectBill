9a113a32768cbc0a3218aeabc3f4212bc64f3986
김상현 ( SangHyun Kim ) <66824080+tticjswo@users.noreply.github.com>
2022-06-04 19:56:49 +0900

    Update settings.py

--- reborn/settings.py
+++ reborn/settings.py
@@ -97,21 +97,21 @@ CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'reborn.urls'
 
 TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [
-#             BASE_DIR / 'templates',
-#         ],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'reborn.wsgi.application'
