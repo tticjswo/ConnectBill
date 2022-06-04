f27c0ffb795bcafd3589b8154891f8a800109e8b
김상현 ( SangHyun Kim ) <66824080+tticjswo@users.noreply.github.com>
2022-06-04 20:00:32 +0900

    Update settings.py

--- reborn/settings.py
+++ reborn/settings.py
@@ -100,7 +100,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
