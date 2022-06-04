60076576a83e157c5f82f947d484e1868485036b
김상현 ( SangHyun Kim ) <66824080+tticjswo@users.noreply.github.com>
2022-06-04 18:14:10 +0900

    Update settings.py

--- reborn/settings.py
+++ reborn/settings.py
@@ -169,7 +169,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:3000' ,'http://localhost:3000',
     'http://127.0.0.1:8000' ,'http://localhost:8000',
-    'https://connectbill-frontend-soongsil.vercel.app/'
+    'https://connectbill-frontend-soongsil.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
