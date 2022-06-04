4565d09161a79485618c6fb43106015a906d503a
tticjswo <tticjswo@naver.com>
2022-06-04 21:57:52 +0900

    no message

--- users/utils.py
+++ users/utils.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import Designer,Client
 
-default_profile_image =  'bucketforconnectbill/profile_image/'
+default_profile_image =  'https://bucketforconnectbill.s3.ap-northeast-2.amazonaws.com/profile_image/user_default_image.png'
 
 def get_and_authenticate_user(username, password):
     user = authenticate(username=username, password=password)
